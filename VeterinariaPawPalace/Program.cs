ba4aa13830dafa652eb069f5b0e31a7b608df856
FausssG <128431695+FausssG@users.noreply.github.com>
2023-06-19 15:31:37 -0300

    Update Program.cs
    hola

--- VeterinariaPawPalace/Program.cs
+++ VeterinariaPawPalace/Program.cs
@@ -6,7 +6,7 @@ namespace VeterinariaPawPalace
     {
         static void Main(string[] args)
         {
-            Cliente c1 = new Cliente("12341234", "matias", "perez", new Domicilio("pacheco", 201));
+            Cliente c1 = new Cliente("448596", "fau", "g", new Domicilio("pacheco", 201));
             Console.WriteLine(c1);
             Mascota m1 = new Mascota(1, DateTime.Now, "pepe", Sexo.MACHO, c1);
             Console.WriteLine(m1);
