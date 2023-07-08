38c93c54ee393a451311930b9d20721aa952fdc4
Matias Bernardi <matias.bernardi.1@gmail.com>
2023-07-08 17:07:51 -0300

    Inicio de Desarrollo de clases
    Clases agregadas: raza, especie.
    Añadido de campos y propiedades en mascota

---
+++ Clases/Raza.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Raza
+    {
+        #region Propiedades
+
+        private int cod;
+        public int Cod { get => cod; set => cod = value; }
+
+        private string nombre;
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        private Especie especieRaza;
+        public Especie EspecieRaza { get => especieRaza; set => especieRaza = value;}
+
+        #endregion
+    }
+}
--- VeterinariaPawPalace/Program.cs
+++ VeterinariaPawPalace/Program.cs
@@ -8,7 +8,7 @@ namespace VeterinariaPawPalace
         {
             Cliente c1 = new Cliente("448596", "fau", "g", new Domicilio("pacheco", 201));
             Console.WriteLine(c1);
-            Mascota m1 = new Mascota(1, DateTime.Now, "pepe", Sexo.MACHO, c1);
+            Mascota m1 = new Mascota(1, DateTime.Now, "pepe", Mascota.Sexos.MACHO, c1);
             Console.WriteLine(m1);
         }
     }
